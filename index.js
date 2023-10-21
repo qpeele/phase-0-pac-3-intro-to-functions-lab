2f86389b6a371876c3e544081d873f4b71310b1b
Quachell Peele <quachellpeele@gmail.com>
2023-10-21 13:21:19 -0400

    invitation to dinner

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+     //'Hello!'
+  return string.toUpperCase(); 
+}
+function whisper(string){
+    //'Hello!'
+return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+
+
+
+
+
+
+
+function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase()=== string){
+return "I can't hear you!";
+}
+
+if (string.toUpperCase()=== string){
+    return "YES INDEED!";
+    }
+
+
+
+    if (string===  "Let's have dinner together!"){
+        return "I would love to!";
+        }
+
+
+
+
+
+}
